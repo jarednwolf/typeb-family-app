478ea01d1dfc5f1eae83b4e88fae37681da54dd5
Jared Wolf <jaredwolf@Jareds-MacBook-Pro.local>
2025-11-02 22:16:08 -0700

    ios(build): temporarily disable Sentry native linking via react-native.config.js to resolve build errors

--- react-native.config.js
+++ react-native.config.js
@@ -2,6 +2,8 @@ module.exports = {
   dependencies: {
     // Temporarily disable RevenueCat native linking on iOS to unblock build
     'react-native-purchases': { platforms: { ios: null } },
+    // Temporarily disable Sentry native linking on iOS to bypass C++ build issues on CI image
+    '@sentry/react-native': { platforms: { ios: null } },
   },
 };
 
